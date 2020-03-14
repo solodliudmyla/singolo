a5f432e1cf37fc42cdea2840401706e6ba51546d
Solod Liudmyla <solodludmila@gmail.com>
2020-03-14 10:30:27 +0200

    feat: slide switch

--- script.js
+++ script.js
@@ -10,3 +10,31 @@ for (let i = 0; i < navItems.length; i++) {
     this.className += " active";
   });
 }
+
+
+//  Slider----------------------------------------------------------------------
+
+let backgroundColorsSlider=['#ef6c64','#0000FF'];
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+  changeBackgroundColor(slideIndex)
+}
+function changeBackgroundColor(n) {
+  let slider= document.querySelector('#slider');
+  slider.style.backgroundColor=backgroundColorsSlider[n-1];
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slides[slideIndex-1].style.display = "block";
+}
