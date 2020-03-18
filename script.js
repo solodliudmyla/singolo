7e0ea648cffe78b17e3d992e38fae6921b8ebba6
Solod Liudmyla <solodludmila@gmail.com>
2020-03-18 20:20:47 +0200

    add modal window template

--- script.js
+++ script.js
@@ -95,3 +95,32 @@ console.log(k[i].style.border);
     e.currentTarget.style.border='5px solid rgb(240, 108, 100)'
   }
 }
+
+//----modal window-------------------------------------------------------------------------------------------------------
+
+// Get DOM Elements
+const modal = document.querySelector('#my-modal');
+const modalBtn = document.querySelector('#modal-btn');
+const closeBtn = document.querySelector('.close');
+
+// Events
+modalBtn.addEventListener('click', openModal);
+closeBtn.addEventListener('click', closeModal);
+window.addEventListener('click', outsideClick);
+
+// Open
+function openModal() {
+  modal.style.display = 'block';
+}
+
+// Close
+function closeModal() {
+  modal.style.display = 'none';
+}
+
+// Close If Outside Click
+function outsideClick(e) {
+  if (e.target === modal) {
+    modal.style.display = 'none';
+  }
+}
